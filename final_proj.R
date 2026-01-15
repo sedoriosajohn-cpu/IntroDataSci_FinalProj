73d35eb365c130b1b54dea1360c4359c7a8e3c66
John Sedoriosa <sedoriosajohn@gmail.com>
2026-01-15 12:47:54 -0500

    Update final_proj.R

--- final_proj.R
+++ final_proj.R
@@ -1,3 +1,9 @@
 library(tidyverse)
-df <- read_csv("C:\\Users\\johnsedoriosa\\Downloads\\scfp2022excel\\SCFP2022.csv")
+
+#reading dataset
+df <- read_csv("\\Users\\johnsedoriosa\\Downloads\\scfp2022excel\\SCFP2022.csv")
 df
+
+#cleaning dataset
+df_clean <- df |> filter(Y1 %% 10 ==1)
+df_clean
