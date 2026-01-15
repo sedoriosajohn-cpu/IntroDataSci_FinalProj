eedb47533bc806907c31012808cde8e5e6dde4dd
John Sedoriosa <sedoriosajohn@gmail.com>
2026-01-15 13:40:30 -0500

    basic EDA stuff

--- final_proj.R
+++ final_proj.R
@@ -1,3 +1,18 @@
 library(tidyverse)
 df <- read_csv("C:\\Users\\johnsedoriosa\\Downloads\\scfp2022excel\\SCFP2022.csv")
 df
+
+#cleaning dataset
+df_clean <- df |> filter(Y1 %% 10 ==1) #filters every 10 so same house isn't counted multiple times
+df_clean
+
+#EDA stuff
+print(dim(df_clean))
+print(names(df_clean))
+print(str(df_clean))
+print(head(df_clean))
+
+##basic analysis of income and wealth
+summary(df_clean$INCOME)
+quantile(df_clean$NETWORTH, probs = c(0.25, 0.5, 0.75, 0.9))
+
