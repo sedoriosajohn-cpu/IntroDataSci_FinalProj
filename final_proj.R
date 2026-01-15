c04984895aa59dc672a91c3654a2a48d112e5c6d
John Sedoriosa <sedoriosajohn@gmail.com>
2026-01-15 13:15:13 -0500

    Update final_proj.R

--- final_proj.R
+++ final_proj.R
@@ -5,5 +5,15 @@ df <- read_csv("\\Users\\johnsedoriosa\\Downloads\\scfp2022excel\\SCFP2022.csv")
 df
 
 #cleaning dataset
-df_clean <- df |> filter(Y1 %% 10 ==1)
+df_clean <- df |> filter(Y1 %% 10 ==1) #filters every 10 so same house isn't counted multiple times
 df_clean
+
+#keeping variables that we'll use for project
+retire <- df_clean |> 
+  select(YY1, Y1, OCCAT1, OCCAT2, RACE, LIFECL, 
+         INCOME, SAVED, FINLIT, IRAKH, THRIFT, 
+         CCBAL, EDUC, HOUSECL,ASSET, SAVING, CHECKING, 
+         EQUITY, VEHIC, HBROK, NOWN, NLEASE, DEBT, RENT,
+         BNPL, TPAY, REVPAY, MORTPAY, CONSPAY, KNOWL, 
+         FOODHOME, FOODDELV, FOODAWAY)
+retire
