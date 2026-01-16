679cdccd04e7a175e46efc9ef136eff09abb15c5
John Sedoriosa <sedoriosajohn@gmail.com>
2026-01-16 12:35:40 -0500

    added more eda stuff

--- final_proj.R
+++ final_proj.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(skimr)
 df <- read_csv("C:\\Users\\johnsedoriosa\\Downloads\\scfp2022excel\\SCFP2022.csv")
 df
 
@@ -15,4 +16,12 @@ print(head(df_clean))
 ##basic analysis of income and wealth
 summary(df_clean$INCOME)
 quantile(df_clean$NETWORTH, probs = c(0.25, 0.5, 0.75, 0.9))
+
+colSums(is.na(df_clean)) #checks for missing values in each column
+
+prop.table(table(df_clean$SAVED)) #shows percentage of households who saved
+
+skim(df_clean) #summaries of every column and gives mini-histograms 
+
+table(df_clean$EDUC, df_clean$SAVED)
 
